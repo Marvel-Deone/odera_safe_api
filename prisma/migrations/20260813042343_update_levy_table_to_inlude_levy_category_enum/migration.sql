817ef270cdd5d8fa6c7ab47424886ba86b5fe3b2
Folagbade <89777325+Marvel-Deone@users.noreply.github.com>
2026-08-13 11:34:53 +0100

    Update migration.sql

--- prisma/migrations/20260813042343_update_levy_table_to_inlude_levy_category_enum/migration.sql
+++ prisma/migrations/20260813042343_update_levy_table_to_inlude_levy_category_enum/migration.sql
@@ -1,21 +1,48 @@
-/*
-  Warnings:
+-- /*
+--   Warnings:
 
-  - Changed the type of `category` on the `Levy` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
+--   - Changed the type of `category` on the `Levy` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
 
-*/
--- CreateEnum
-CREATE TYPE "LevyCategory" AS ENUM ('ADMIN_LEVY', 'MONTHLY_RESIDENT_LEVY');
+-- */
+-- -- CreateEnum
+-- CREATE TYPE "LevyCategory" AS ENUM ('ADMIN_LEVY', 'MONTHLY_RESIDENT_LEVY');
+
+-- -- DropForeignKey
+-- ALTER TABLE "Wallet" DROP CONSTRAINT "Wallet_residentId_fkey";
+
+-- -- AlterTable
+-- ALTER TABLE "Levy" DROP COLUMN "category",
+-- ADD COLUMN     "category" "LevyCategory" NOT NULL;
+
+-- -- CreateIndex
+-- CREATE UNIQUE INDEX "Levy_estateId_category_period_key" ON "Levy"("estateId", "category", "period");
+
+-- -- AddForeignKey
+-- ALTER TABLE "Wallet" ADD CONSTRAINT "Wallet_residentId_fkey" FOREIGN KEY ("residentId") REFERENCES "Resident"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
 
 -- DropForeignKey
 ALTER TABLE "Wallet" DROP CONSTRAINT "Wallet_residentId_fkey";
 
 -- AlterTable
-ALTER TABLE "Levy" DROP COLUMN "category",
-ADD COLUMN     "category" "LevyCategory" NOT NULL;
+ALTER TABLE "Levy"
+ALTER COLUMN "category" TYPE "LevyCategory"
+USING (
+    CASE
+        WHEN "category" = 'MONTHLY_RESIDENT_LEVY'
+            THEN 'MONTHLY_RESIDENT_LEVY'::"LevyCategory"
+        ELSE 'ADMIN_LEVY'::"LevyCategory"
+    END
+);
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Levy_estateId_category_period_key" ON "Levy"("estateId", "category", "period");
+CREATE UNIQUE INDEX "Levy_estateId_category_period_key"
+ON "Levy"("estateId", "category", "period");
 
 -- AddForeignKey
-ALTER TABLE "Wallet" ADD CONSTRAINT "Wallet_residentId_fkey" FOREIGN KEY ("residentId") REFERENCES "Resident"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "Wallet"
+ADD CONSTRAINT "Wallet_residentId_fkey"
+FOREIGN KEY ("residentId")
+REFERENCES "Resident"("id")
+ON DELETE SET NULL
+ON UPDATE CASCADE;
